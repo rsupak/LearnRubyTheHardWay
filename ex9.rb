25dd85781c652ab16fa60bb4e8d2186f7de63536
Richard Supak <richard.supak@icloud.com>
2018-09-18 10:35:34 -0500

    Add first commit

---
+++ ex9.rb
@@ -0,0 +1,12 @@
+days = "Mon Tues Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec\n"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts %q{
+There's something going on here.
+With this weird quote
+We'll be able to type as much as we like
+Even 4 lines if we want, or 5, or 6.
+}
